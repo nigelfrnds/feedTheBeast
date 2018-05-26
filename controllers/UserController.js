50e0f89baecb6c1e601597c3e17be2555059acaf
Nigel Fernandes <fernandes.nigelj@gmail.com>
2018-05-26 01:43:05 -0400

    [NF] Created register/login functions

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,13 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports.registerUser = async (req, res) => {
     try {
         console.log('registerUser: ', req.body);
-        // hash passwords
-        // store to db
-        // use jsonwebtoken to sign user
-        // send signed user as response
-        res.status(200).send({ message: 'registerUser' });
+        const { email, password } = req.body;
+        let user = await User.createUser({ email, password });
+        let token = await jwt.sign({ user }, process.env.SECRET_KEY);
+        res.status(200).send({ user, token });
     } catch (error) {
         console.log('registerUser error: ', error);
         res.status(400).send({ message: error });
@@ -17,7 +17,21 @@ module.exports.registerUser = async (req, res) => {
 module.exports.loginUser = async (req, res) => {
     try {
         console.log('loginUser: ', req.body);
-        // similar to register
+        const { email, password } = req.body;
+
+        let existingUser = await User.findOne({ email });
+        if(existingUser === null) {
+            return res.status(400).send({ message: 'User does not exists' });
+        }
+
+        let isMatch =  await User.comparePassword(password, existingUser.password);
+        
+        if(isMatch) {
+            let token = await jwt.sign({ user: existingUser }, process.env.SECRET_KEY);
+            res.status(200).send({ user: existingUser, token });
+        } else {
+            res.status(400).send({ message: 'Incorrect email/password' });
+        }
         res.status(200).send({ message: 'loginUser' });
     } catch (error) {
         console.log('loginUser error: ', error);
